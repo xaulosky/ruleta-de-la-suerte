b6fc8e3727337363364be99abfe08cca0f131f0f
Jota Zen <jotazents@gmail.com>
2023-04-28 19:01:28 -0400

    PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        navigateFallbackDenylist: [/^\/api/],
+      },
+      manifest: {
+        "name": "Ruleta-de-la-suerte",
+        "version": "0.1",
+        "short_name": "Ruleta",
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "lang": "es",
+        "scope": "/",
+        "icons": [
+          {
+            "src": "icon.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          }
+        ]
+      }
+    }),
+  ],
 })
